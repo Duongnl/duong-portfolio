5a21e8ada6936b857ffd0f35f58b8732e955b0fe
Duongg <lengocduong003@gmail.com>
2025-02-18 16:07:01 +0700

    init

---
+++ constants/EducationData.js
@@ -0,0 +1,26 @@
+export const EducationData = [
+    {
+        name: "Sai Gon University",
+        image:
+            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219056/edekjqivqlnsyg8pfmxw.png",
+        // schoolOrCollege: "GOVERNMENT COLLEGE, KOTA",
+        fromTo: "2021 - 2026",
+        statusOrPrecentage: "Studying",
+    },
+    {
+        name: "Quang Tri Town High School",
+        image:
+            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/jkjfvdlxjntnyrxs57ia.png",
+        // schoolOrCollege: "SWAMI TEONRAM ALOK SR. SEC. SCHOOL DADABARI, KOTA",
+        fromTo: "2018 - 2021",
+        statusOrPrecentage: "Compeleted",
+    },
+    // {
+    //     name: "Secondary Examination",
+    //     image:
+    //         "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/tnverjapfr4slg7fnfcw.png",
+    //     schoolOrCollege: "A.V.M. SECONDARY SCHOOL, RAWATBHATA",
+    //     fromTo: "2019",
+    //     statusOrPrecentage: "93.33%",
+    // },
+];
---
+++ constants/ProjectsData.js
@@ -0,0 +1,29 @@
+export const ProjectsData = [
+    {
+        projectName: "Shop",
+        // liveUrl: "https://shovee.com",
+        githubUrl: "https://github.com/Duongnl/Web2",
+        projectImage: {
+            imageUrl: "https://res.cloudinary.com/moment-images/image/upload/Picture1_n8keq9"
+        },
+        techs: ["PHP", "Boostrap", "Mysql"]
+    },
+    {
+        projectName: "Tour reservation",
+        // liveUrl: "https://youtube-clone-shivraj.vercel.app",
+        githubUrl: "https://github.com/Duongnl/tour_reservation",
+        projectImage: {
+            imageUrl: "https://res.cloudinary.com/moment-images/image/upload/Picture2_zufnqv"
+        },
+        techs: ["Boostrap", "Springboot", "Mysql"]
+    },
+    {
+        projectName: "Weather forecast",
+        liveUrl: "https://weather-website-lac-nine.vercel.app",
+        githubUrl: "https://github.com/Duongnl/weather_website",
+        projectImage: {
+            imageUrl: "https://res.cloudinary.com/moment-images/image/upload/Screenshot_2025-02-18_152720_ykgkti"
+        },
+        techs: ["Nextjs", "TypeScript", "OpenWeatherMap", "ContextAPI"]
+    }
+]
